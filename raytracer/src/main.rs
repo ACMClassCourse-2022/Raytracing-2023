876735778f4dc6ffaad9e099fd0fe134b660c04c
hnyls2002 <hnyls2002@gmail.com>
2023-06-29 16:04:23 +0800

    ✨ feat(main): skeleton

---
+++ raytracer/src/main.rs
@@ -0,0 +1,42 @@
+use console::style;
+use image::{ImageBuffer, RgbImage};
+use indicatif::ProgressBar;
+use std::{fs::File, process::exit};
+
+fn main() {
+    let path = "output/image1.jpg";
+
+    let width = 256;
+    let height = 256;
+    let quality = 100;
+    let mut img: RgbImage = ImageBuffer::new(width, height);
+
+    let progress = if option_env!("CI").unwrap_or_default() == "true" {
+        ProgressBar::hidden()
+    } else {
+        ProgressBar::new((height * width) as u64)
+    };
+
+    for j in (0..height).rev() {
+        for i in 0..width {
+            let pixel = img.get_pixel_mut(i, j);
+
+            let r: f64 = (i as f64) / ((width - 1) as f64) * 255.999;
+            let g: f64 = (j as f64) / ((height - 1) as f64) * 255.999;
+            let b: f64 = 0.25 * 255.999;
+            *pixel = image::Rgb([r as u8, g as u8, b as u8]);
+        }
+        progress.inc(1);
+    }
+    progress.finish();
+
+    println!("Ouput image as \"{}\"", style(path).yellow());
+    let output_image = image::DynamicImage::ImageRgb8(img);
+    let mut output_file = File::create(path).unwrap();
+    match output_image.write_to(&mut output_file, image::ImageOutputFormat::Jpeg(quality)) {
+        Ok(_) => {}
+        Err(_) => println!("{}", style("Outputting image fails.").red()),
+    }
+
+    exit(0);
+}
